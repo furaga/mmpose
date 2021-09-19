1b76dfaf6d379ad7ceba554209b731c03735f01c
furaga <furaga.fukahori@gmail.com>
2021-09-19 19:58:23 +0900

    convert unity annotation to crowdpose format

--- tools/dataset/unity_to_crowdpose.py
+++ tools/dataset/unity_to_crowdpose.py
@@ -17,6 +17,7 @@ def parse_args():
         "--list_path", type=Path, default=Path("data/anim_data_list.txt")
     )
     parser.add_argument("--out_dir", type=Path, default=Path("out"))
+    parser.add_argument("--type", type=str, default="train")
     args = parser.parse_args()
     return args
 
@@ -39,6 +40,45 @@ def load_data_list(list_path):
     return data_list
 
 
+def get_kps_bbox(person, width, height):
+    keypoints = np.array(person["keypoints"]).astype(int)
+    keypoints = keypoints.reshape((14, 3))
+    n_keypoints = 0
+    new_keypoints = []
+    for x, y, w in keypoints:
+        valid = True
+        if w <= 0:
+            valid = False
+        if x < 0 or width <= w or y < 0 or height <= y:
+            valid = False
+        if valid:
+            new_keypoints.append((x, y, w))
+            n_keypoints += 1
+        else:
+            new_keypoints.append((0, 0, 0))
+    new_keypoints = np.array(new_keypoints)
+    n_keypoints -= 2
+
+    xmin = np.min(new_keypoints[:, 0])
+    xmax = np.max(new_keypoints[:, 0])
+    ymin = np.min(new_keypoints[:, 1])
+    ymax = np.max(new_keypoints[:, 1])
+    cx = (xmin + xmax) / 2
+    cy = (ymin + ymax) / 2
+    dx = cx - xmin
+    dy = cy - ymin
+    ratio = 1.1
+    bbox = [
+        float(cx - dx * ratio),
+        float(cx + dx * ratio),
+        float(cy - dy * ratio),
+        float(cy + dy * ratio),
+    ]
+
+    new_keypoints = [int(v) for v in new_keypoints.ravel()]
+    return n_keypoints, new_keypoints, bbox
+
+
 def make_crowdpose_json(annotations):
     with open("data/crowdpose_base.json") as f:
         d = json.loads(f.read())
@@ -57,18 +97,20 @@ def make_crowdpose_json(annotations):
         )
 
         for person in ann["people"]:
+            n_keypoints, keypoints, bbox = get_kps_bbox(person, width, height)
             d["annotations"].append(
                 {
-                    "num_keypoints": 0,
+                    "num_keypoints": n_keypoints,
                     "iscrowd": 0,
-                    "keypoints": 0,
+                    "keypoints": keypoints,
                     "image_id": ann["image_id"],
-                    "bbox": 0,
+                    "bbox": bbox,
                     "category_id": 1,
                     "id": annot_id,
                 }
             )
             annot_id += 1
+
     return d
 
 
@@ -88,7 +130,7 @@ def main(args):
 
             img_path = img_dir / image_name
             out_img_path = args.out_dir / f"{image_id:08d}.jpg"
-            shutil.copy(img_path, out_img_path)
+            #shutil.copy(img_path, out_img_path)
 
             people = ann["people"]
             new_ann = {
@@ -99,12 +141,11 @@ def main(args):
             all_annotations.append(new_ann)
             image_counter += 1
 
-            print(new_ann)
-            exit()
-
     d = make_crowdpose_json(all_annotations)
-    with open(args.out_dir / f"mmpose_anim_{args.type}.json", "w") as f:
+    out_json_path = args.out_dir / f"mmpose_anim_{args.type}.json"
+    with open(out_json_path, "w") as f:
         json.dump(d, f)
+        print("Saved", out_json_path)
 
 
 if __name__ == "__main__":
